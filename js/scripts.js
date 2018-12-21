3b0d7bb8fbc94ed851daf5d1cdaebd102c4df93a
EspeIgira <espeigira@gmail.com>
2018-12-21 13:45:52 +0200

    Final result edited

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,12 @@
     $("#blanks form").submit(function(event){
 
       var button1 = parseInt($("input:radio[name=button1]:checked").val());
-      var button2 = $("input:radio[name=button2]:checked").val();
-      var button3 = $("input:radio[name=button3]:checked").val();
+      var button2 = parseInt($("input:radio[name=button2]:checked").val());
+      var button3 = parseInt($("input:radio[name=button3]:checked").val());
 
-      /*alert(button1);
-      alert(button2);
-      alert(button3);*/
-      var testResults = function(button1,button2 ,button3){
-        return button1 + button2 + button3;
-        
-        }
+     
       var result= 0;
-      var message="";
-
-      /*var result = testResults(button1, button2 ,button3);*/
+     
       if (button1===40){
         
         result+=40;
@@ -30,7 +22,7 @@
         
         result+=40;
       }
-      $(".final").show();
+       $(".final").show();
       $("#display").text("Your score is:" + result);
 
      
