18077745cbc73867ec5cf59f780e07f745502b88
EspeIgira <espeigira@gmail.com>
2018-12-21 11:28:13 +0200

    Changed script.js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var button1 = $("input:radio[name=button1]:checked").val();
     var button2 = $("input:radio[name=button2]:checked").val();
@@ -20,4 +20,36 @@ $(document).ready(function() {
 
     event.preventDefault();
   });
-});
+});*/
+var testResults = function(button1,button2 ,button3){
+  return button1 + button2 + button3;
+  
+  }
+  
+  $(document).ready(function(){
+    $(".begin").click(function(){
+      $("#test").fadeToggle("slow");
+    });
+  
+    $("#blanks form").submit(function(event){
+      var button1 = $("input:radio[name=button1]:checked").val();
+      var button2 = $("input:radio[name=button2]:checked").val();
+      var button3 = $("input:radio[name=button3]:checked").val();
+
+      /*alert(button1);
+      alert(button2);
+      alert(button3);*/
+  
+      var result = testResults(button1, button2 ,button3);
+  
+     
+      blanks.forEach(function(blank) {
+        debugger;
+        
+        $("#display").text("Your score is:" + result);
+      });
+  
+      event.preventDefault();
+      $(".final").show();
+    });
+  });
