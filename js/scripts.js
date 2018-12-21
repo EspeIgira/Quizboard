585466d0d18087da3c4a04f318639d2bb404be49
EspeIgira <espeigira@gmail.com>
2018-12-21 13:40:09 +0200

    All HTML,SCRIPT.js and CSS are Correct (Changed)

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,47 @@
-var testResults = function(button1,button2 ,button3){
-  return button1 + button2 + button3;
-  
-  }
-  
-  $(document).ready(function(){
+
+    $(document).ready(function(){
     
     $("#blanks form").submit(function(event){
-      
-      var button1 = $("input:radio[name=button1]:checked").val();
+
+      var button1 = parseInt($("input:radio[name=button1]:checked").val());
       var button2 = $("input:radio[name=button2]:checked").val();
       var button3 = $("input:radio[name=button3]:checked").val();
 
       /*alert(button1);
       alert(button2);
       alert(button3);*/
-  
-      var result = testResults(button1, button2 ,button3);
+      var testResults = function(button1,button2 ,button3){
+        return button1 + button2 + button3;
+        
+        }
+      var result= 0;
+      var message="";
+
+      /*var result = testResults(button1, button2 ,button3);*/
+      if (button1===40){
+        
+        result+=40;
+      }
+      if (button2===40){
+        
+        result+=40;
+      } 
+      if (button3===40){
+        
+        result+=40;
+      }
+      $(".final").show();
+      $("#display").text("Your score is:" + result);
+
+     
+     
+      event.preventDefault();
+      }) 
+    });
+   
+     
+    
   
      
-        $("#display").text("Your score is:" + result);
-      
-  
-      event.preventDefault();
-      $(".final").show();
-    });
-  });
-
+   
 
